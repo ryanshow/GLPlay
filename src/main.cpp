32268c47086ba0958005571fa4e78cbd8855067b
Ryan Showalter <ryanshow@gmail.com>
2016-09-26 23:29:18 -0500

    Get the project compiling on OSX

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <glbinding/gl/gl.h>
 #include <glbinding/Binding.h>
 #include <GLFW/glfw3.h>
-#include <glm/gtc/constants.hpp>
 #include <thread>
 #include <fstream>
 #include <iostream>
@@ -16,16 +15,20 @@ static const double MAX_FPS_INTERVAL = (1/25.0f);
 int main() {
     GLFWwindow *window;
 
-    std::cout << "Is the main function being entered?" << std::endl;
-
-
     if(!glfwInit()) {
         std::cout << "GLFW failed to init" << std::endl;
         exit(EXIT_FAILURE);
     }
 
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#else
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+#endif
 
     window = glfwCreateWindow(640, 480, "GLPlay", nullptr, nullptr);
     if(!window) {
@@ -38,16 +41,60 @@ int main() {
     glbinding::Binding::initialize();
     glfwSwapInterval(1);
 
-    std::cout << "pi = " << glm::pi<float>() << std::endl;
+    // === Load the font ===
+
+    const char* ttf_filename = "assets/DroidSansMono.ttf";
+    GLuint ftex;
+
+    std::ifstream is (ttf_filename, std::ifstream::binary);
+
+    if (!is) {
+        std::cout << "Unable to open " << ttf_filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // determine the length of the file
+    is.seekg(0, is.end);
+    int length = static_cast<int>(is.tellg());
+    is.seekg(0, is.beg);
+
+    char *ttf_buffer = new char[length];
+
+    std::cout << "Reading " << length << " bytes from " << ttf_filename << std::endl;
+
+    is.read(ttf_buffer, length);
+    is.close();
+
+    unsigned char temp_bitmap[512 * 512];
+
+    stbtt_bakedchar cdata[96];
+
+    stbtt_BakeFontBitmap(reinterpret_cast<unsigned char *>(ttf_buffer), 0, 48.0, temp_bitmap, 512, 512, 32, 95, cdata);
+
+    glGenTextures(1, &ftex);
+
+    //glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, ftex); {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 512, 512, 0, GL_RED, GL_UNSIGNED_BYTE, temp_bitmap);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
 
     // Begin Triangle Rendering Test
 
     GLfloat vertices[] = {
-        // Positions          // Colors           // Texture Coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f,   // Top Right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f,   // Bottom Right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f,   // Bottom Left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f    // Top Left
+        // Positions         // Texture Coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f,   // Top Right
+        0.5f, -0.5f, 0.0f,   1.0f, 1.0f,   // Bottom Right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,   // Bottom Left
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f    // Top Left
     };
 
     GLuint indices[] = {  // Note that we start from 0!
@@ -65,8 +112,7 @@ int main() {
 
     const GLchar* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 inPosition;\n"
-    "layout (location = 1) in vec3 inColor;\n"
-    "layout (location = 2) in vec2 inTexCoord;\n"
+    "layout (location = 1) in vec2 inTexCoord;\n"
     "out vec2 texCoord;\n"
     "void main() {\n"
     "  gl_Position = vec4(inPosition, 1.0);\n"
@@ -83,7 +129,7 @@ int main() {
     "out vec4 outColor;\n"
     "uniform sampler2D tex0;\n"
     "void main() {\n"
-    "  outColor = vec4(0.0, 0.0, 0.0, texture(tex0, texCoord).r);\n"
+    "  outColor = vec4(1.0, 1.0, 0.0, texture(tex0, texCoord).r);\n"
     "}\0";
 
     GLuint fragmentShader;
@@ -110,67 +156,17 @@ int main() {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
         glEnableVertexAttribArray(1);
 
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-        glEnableVertexAttribArray(2);
-
-        //glBindVertexArray(0);
+        glBindVertexArray(0);
     }
 
     // End Triangle Font Rendering Test
 
-    // Begin Font Loading Test
-
-    const char* ttf_filename = "assets/DroidSansMono.ttf";
-    GLuint ftex;
-
-    std::ifstream is (ttf_filename, std::ifstream::binary);
-    if (is) {
-        // determine the length of the file
-        is.seekg(0, is.end);
-        int length = static_cast<int>(is.tellg());
-        is.seekg(0, is.beg);
-
-        char *ttf_buffer = new char[length];
-
-        std::cout << "Reading " << length << " bytes from " << ttf_filename << std::endl;
-
-        is.read(ttf_buffer, length);
-        is.close();
-
-        unsigned char temp_bitmap[512 * 512];
-
-        stbtt_bakedchar cdata[96];
-
-        stbtt_BakeFontBitmap(reinterpret_cast<unsigned char *>(ttf_buffer), 0, 32.0, temp_bitmap, 512, 512, 32, 96, cdata);
-
-
-        glGenTextures(1, &ftex);
-
-        //glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, ftex); {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 512, 512, 0, GL_RED, GL_UNSIGNED_BYTE, temp_bitmap);
-            glGenerateMipmap(GL_TEXTURE_2D);
-
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
-
-        // delete[] temp_bitmap;
-
-    } else {
-        std::cout << "Unable to open " << ttf_filename << std::endl;
-    }
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
